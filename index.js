181d34a6e3adaf1dc59f7f87514700becda17ca5
Ezequiel-H <horowitzeze@gmail.com>
2023-06-11 15:33:05 -0300

    adding endpoint health

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/extensions */
 import pkg from 'telegraf';
+import express from 'express';
 import router from './routes.js';
 
 import {} from 'dotenv/config';
@@ -9,5 +10,20 @@ const { Telegraf } = pkg;
 
 const config = process.env;
 const bot = new Telegraf(config.BOT_TOKEN);
+
+const app = express();
+
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const { port } = server.address();
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+app.get('/healthz', (_, res) => {
+  res.send({
+    message: 'OK',
+  });
+});
 
 router(bot);
